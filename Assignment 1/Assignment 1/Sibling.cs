801325d079813aabfd6c3b19c83f2dfefd6086b7
Sadain Iqrar Abbasi <sadain.abbasi@gmail.com>
2016-03-25 19:00:54 +0500

    Assignment 1 Visual Programming

---
+++ Assignment 1/Assignment 1/Sibling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Sibling
+    {
+        private string years;
+        private string month;
+        private string days;
+        private DateTime age;
+
+        public Sibling(DateTime dob)
+        {
+            DateTime presentYear = DateTime.Now;
+            TimeSpan ts = presentYear - dob;
+            age = new DateTime() + ts;
+            years = (age.Year-1).ToString();
+            month = (age.Month-1).ToString();
+            days = (age.Day-1).ToString();
+        }
+        
+        public void showAge()
+        {
+            Console.WriteLine(" {0} Years {1} Month {2} Days", years, month, days);
+        }
+        public DateTime getAge()
+        {
+            return age;
+        }
+
+    }
+}
