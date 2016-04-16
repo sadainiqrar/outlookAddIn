443c1d080da0f700735e5d79e86d8e4a36bd4df3
sadainiqrar <sadain.abbasi@gmail.com>
2016-04-16 12:27:30 -0700

    Working with One Drive SDK

--- LiveSyncWindowsForm/LiveSyncWindowsForm/MainPage.Designer.cs
+++ LiveSyncWindowsForm/LiveSyncWindowsForm/MainPage.Designer.cs
@@ -28,114 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.uploadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.uploadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.createFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.downloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveSelectedFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip2.SuspendLayout();
+            this.filesBtn = new System.Windows.Forms.Button();
+            this.EventsBtn = new System.Windows.Forms.Button();
+            this.contactsBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // menuStrip2
+            // filesBtn
             // 
-            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.uploadToolStripMenuItem,
-            this.contactsToolStripMenuItem,
-            this.downloadToolStripMenuItem});
-            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(305, 24);
-            this.menuStrip2.TabIndex = 0;
-            this.menuStrip2.Text = "menuStrip2";
+            this.filesBtn.Location = new System.Drawing.Point(3, 29);
+            this.filesBtn.Name = "filesBtn";
+            this.filesBtn.Size = new System.Drawing.Size(75, 23);
+            this.filesBtn.TabIndex = 0;
+            this.filesBtn.Text = "Files";
+            this.filesBtn.UseVisualStyleBackColor = true;
+            this.filesBtn.Click += new System.EventHandler(this.filesBtn_Click);
             // 
-            // fileToolStripMenuItem
+            // EventsBtn
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.signOutToolStripMenuItem,
-            this.exitToolStripMenuItem,
-            this.toolStripMenuItem2});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.EventsBtn.Location = new System.Drawing.Point(3, 78);
+            this.EventsBtn.Name = "EventsBtn";
+            this.EventsBtn.Size = new System.Drawing.Size(75, 23);
+            this.EventsBtn.TabIndex = 1;
+            this.EventsBtn.Text = "Events";
+            this.EventsBtn.UseVisualStyleBackColor = true;
+            this.EventsBtn.Click += new System.EventHandler(this.EventsBtn_Click);
             // 
-            // signOutToolStripMenuItem
+            // contactsBtn
             // 
-            this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
-            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.signOutToolStripMenuItem.Text = "Sign Out";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // uploadToolStripMenuItem
-            // 
-            this.uploadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.uploadFileToolStripMenuItem,
-            this.createFolderToolStripMenuItem});
-            this.uploadToolStripMenuItem.Name = "uploadToolStripMenuItem";
-            this.uploadToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
-            this.uploadToolStripMenuItem.Text = "Upload";
-            // 
-            // uploadFileToolStripMenuItem
-            // 
-            this.uploadFileToolStripMenuItem.Name = "uploadFileToolStripMenuItem";
-            this.uploadFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.uploadFileToolStripMenuItem.Text = "Upload File";
-            // 
-            // createFolderToolStripMenuItem
-            // 
-            this.createFolderToolStripMenuItem.Name = "createFolderToolStripMenuItem";
-            this.createFolderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.createFolderToolStripMenuItem.Text = "Create Folder";
-            // 
-            // contactsToolStripMenuItem
-            // 
-            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
-            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
-            this.contactsToolStripMenuItem.Text = "Contacts";
-            // 
-            // downloadToolStripMenuItem
-            // 
-            this.downloadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.saveSelectedFileToolStripMenuItem});
-            this.downloadToolStripMenuItem.Name = "downloadToolStripMenuItem";
-            this.downloadToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
-            this.downloadToolStripMenuItem.Text = "Download";
-            // 
-            // saveSelectedFileToolStripMenuItem
-            // 
-            this.saveSelectedFileToolStripMenuItem.Name = "saveSelectedFileToolStripMenuItem";
-            this.saveSelectedFileToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
-            this.saveSelectedFileToolStripMenuItem.Text = "Save Selected file";
-            // 
-            // toolStripMenuItem2
-            // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
-            this.toolStripMenuItem2.Text = "Sync";
-            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            this.contactsBtn.Location = new System.Drawing.Point(125, 29);
+            this.contactsBtn.Name = "contactsBtn";
+            this.contactsBtn.Size = new System.Drawing.Size(75, 23);
+            this.contactsBtn.TabIndex = 2;
+            this.contactsBtn.Text = "Contacts";
+            this.contactsBtn.UseVisualStyleBackColor = true;
+            this.contactsBtn.Click += new System.EventHandler(this.contactsBtn_Click);
             // 
             // MainPage
             // 
-            this.ClientSize = new System.Drawing.Size(305, 171);
-            this.Controls.Add(this.menuStrip2);
-            this.MainMenuStrip = this.menuStrip2;
-            this.MinimumSize = new System.Drawing.Size(240, 0);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.contactsBtn);
+            this.Controls.Add(this.EventsBtn);
+            this.Controls.Add(this.filesBtn);
             this.Name = "MainPage";
-            this.menuStrip2.ResumeLayout(false);
-            this.menuStrip2.PerformLayout();
+            this.Text = "Live Sync";
+            this.Load += new System.EventHandler(this.MainPage_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -154,5 +91,11 @@
         private System.Windows.Forms.ToolStripMenuItem downloadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveSelectedFileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Button myFilesBtn;
+        private System.Windows.Forms.Button sharedFilesBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button filesBtn;
+        private System.Windows.Forms.Button EventsBtn;
+        private System.Windows.Forms.Button contactsBtn;
     }
 }
--- LiveSyncWindowsForm/LiveSyncWindowsForm/MainPage.cs
+++ LiveSyncWindowsForm/LiveSyncWindowsForm/MainPage.cs
@@ -22,5 +22,37 @@ namespace LiveSyncWindowsForm
 
         }
 
+        private void sharedFilesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void myFilesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EventsBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactsBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
     }
 }
