8383b3ee867aad5eed058fd0ff578805f453b48b
sadainiqrar <sadain.abbasi@gmail.com>
2016-04-23 23:31:21 -0700

    Outlook Addin Attachments Downloads Code

---
+++ LiveSync2.0/LiveSync2.0/LiveSyncRibbon.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace LiveSync2._0
+{
+    partial class LiveSyncRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public LiveSyncRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LiveSyncTab = this.Factory.CreateRibbonTab();
+            this.LiveSyncG1 = this.Factory.CreateRibbonGroup();
+            this.DownloadBtn = this.Factory.CreateRibbonButton();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.BtnDownloadOneDrive = this.Factory.CreateRibbonButton();
+            this.LiveSyncTab.SuspendLayout();
+            this.LiveSyncG1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LiveSyncTab
+            // 
+            this.LiveSyncTab.Groups.Add(this.LiveSyncG1);
+            this.LiveSyncTab.Label = "LiveSyncTab";
+            this.LiveSyncTab.Name = "LiveSyncTab";
+            // 
+            // LiveSyncG1
+            // 
+            this.LiveSyncG1.Items.Add(this.BtnDownloadOneDrive);
+            this.LiveSyncG1.Items.Add(this.DownloadBtn);
+            this.LiveSyncG1.Label = "LiveSync";
+            this.LiveSyncG1.Name = "LiveSyncG1";
+            // 
+            // DownloadBtn
+            // 
+            this.DownloadBtn.Label = "Download";
+            this.DownloadBtn.Name = "DownloadBtn";
+            this.DownloadBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.DownloadBtn_Click);
+            // 
+            // BtnDownloadOneDrive
+            // 
+            this.BtnDownloadOneDrive.Label = "DownloadOneDrive";
+            this.BtnDownloadOneDrive.Name = "BtnDownloadOneDrive";
+            this.BtnDownloadOneDrive.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.configBtn_Click);
+            // 
+            // LiveSyncRibbon
+            // 
+            this.Name = "LiveSyncRibbon";
+            this.RibbonType = "Microsoft.Outlook.Explorer, Microsoft.Outlook.Mail.Read";
+            this.Tabs.Add(this.LiveSyncTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.LiveSyncRibbon_Load);
+            this.LiveSyncTab.ResumeLayout(false);
+            this.LiveSyncTab.PerformLayout();
+            this.LiveSyncG1.ResumeLayout(false);
+            this.LiveSyncG1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab LiveSyncTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup LiveSyncG1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton DownloadBtn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton BtnDownloadOneDrive;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal LiveSyncRibbon LiveSyncRibbon
+        {
+            get { return this.GetRibbon<LiveSyncRibbon>(); }
+        }
+    }
+}
---
+++ LiveSync2.0/LiveSync2.0/LiveSyncRibbon.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using Microsoft.Live;
+using Microsoft.OneDrive.Sdk;
+using Microsoft.OneDrive.Sdk.WindowsForms;
+
+namespace LiveSync2._0
+{
+    public partial class LiveSyncRibbon
+    {
+        private const string AadClientId = "b145a917-23f3-483f-8675-87e6f988a999";
+        private const string AadReturnUrl = "http://localhost:777";
+        private const string MsaClientId = "000000004418641D";
+        private const string MsaReturnUrl = "http://login.live.com/oauth20_desktop.srf";
+
+        private static readonly string[] scopes = { "onedrive.readwrite","wl.offline_access","wl.signin"};
+        private const int uploadChunksize = 10 * 1024 * 1024;
+        private IOneDriveClient oneDriveClient { get; set; }
+
+        private void LiveSyncRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private async void configBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            await this.SignIn(ClientType.Consumer);
+
+        }
+/*
+        private async Task<int> UploadFileToOneDrive()
+        {
+            LiveConnectClient liveConnect;
+            try
+            {
+                var authClient = new LiveAuthClient(AadClientId);
+                LiveLoginResult result = await authClient.InitializeAsync(new string[] {"wl.skydrive","wl.skydrive_update" });
+
+                if(result.Status == LiveConnectSessionStatus.Connected)
+                {
+                    liveConnect = new LiveConnectClient(result.Session);
+                   
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                    need to be done
+                 
+
+                }
+            }
+        }
+*/
+
+        private async Task SignIn(ClientType clientType)
+        {
+            if(this.oneDriveClient == null)
+            {
+                this.oneDriveClient = clientType == ClientType.Consumer
+                    ? OneDriveClient.GetMicrosoftAccountClient(
+                        MsaClientId,MsaReturnUrl,scopes,webAuthenticationUi: new FormsWebAuthenticationUi())
+                        :BusinessClientExtensions.GetActiveDirectoryClient(AadClientId,AadReturnUrl);
+            }
+            try
+            {
+                if(!this.oneDriveClient.IsAuthenticated)
+                {
+                    await oneDriveClient.AuthenticateAsync();
+                }
+                
+            }
+            catch(OneDriveException exception)
+            {
+                if(!exception.IsMatch(OneDriveErrorCode.AuthenticationCancelled.ToString()))
+                {
+                    if(exception.IsMatch(OneDriveErrorCode.AuthenticationCancelled.ToString()))
+                    {
+                        System.Windows.Forms.MessageBox.Show("Authentication failed");
+                        var httpProvider = oneDriveClient.HttpProvider as HttpProvider;
+                        httpProvider.Dispose();
+                        this.oneDriveClient = null;
+                        
+                    }
+                    else
+                    {
+                      
+                    }
+                }
+            }
+        }
+        private void DownloadBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook.Application app = new Outlook.Application();
+            Outlook.Accounts acc = app.Session.Accounts;
+            foreach (Outlook.Account ac in acc)
+            {
+                Outlook.MAPIFolder inBox = app.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                Outlook.Items inBoxItems = inBox.Items;
+                Outlook.MailItem newEmail = null;
+                try
+                {
+                    foreach (object collItem in inBoxItems)
+                    {
+                        newEmail = collItem as Outlook.MailItem;
+                        if (newEmail != null)
+                        {
+                            if (newEmail.Attachments.Count > 0)
+                            {
+                                for (int i = 1; i <= newEmail.Attachments.Count; i++)
+                                {
+                                    newEmail.Attachments[i].SaveAsFile(@"C:\OutlookItems\" + newEmail.Attachments[i].FileName);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string error = (string)ex.Message.Substring(0, 11);
+                    if (error == "Cannot save")
+                    {
+                        System.Windows.Forms.MessageBox.Show("Folder doesnot exist");
+                    }
+                }
+            }
+        }
+    }
+}
